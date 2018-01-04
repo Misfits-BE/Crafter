3d5f05e4eee89838b9a93e51415624adec03f490
Tim Joosten <Tjoosten3@gmail.com>
2018-01-04 13:11:32 +0100

    Fix #3 | Implementatie php-cs-fixer + nitpciks

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
 /**
- * Controller 
- * 
- * De basis controller voor de applicatie. 
- * 
+ * Controller
+ *
+ * De basis controller voor de applicatie.
+ *
  * @author    Tim Joosten <tim@activisme.be>
  * @copyright 2018 Tim Joosten
  */
