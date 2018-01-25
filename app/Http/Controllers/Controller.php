e1a8aa9a4ea29753a1c488f15cf779e44fffdda2
Tim Joosten <Tjoosten3@gmail.com>
2018-01-25 22:29:05 +0100

    Fix #23 | Implementatie trait voor loggin

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Traits\ActivityLog;
 
 /**
  * Controller
@@ -17,5 +18,5 @@ use Illuminate\Routing\Controller as BaseController;
  */
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ActivityLog;
 }
